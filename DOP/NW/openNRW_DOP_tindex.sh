b7a49721d85896237cd311bd85992d80bc82972e
anikaweinmann <aweinmann@mundialis.de>
2023-06-07 22:21:45 +0200

    test 21

--- DOP/NW/openNRW_DOP_tindex.sh
+++ DOP/NW/openNRW_DOP_tindex.sh
@@ -45,6 +45,8 @@ if [ ! -x "`which lynx`" ] ; then
     exit 1
 fi
 
+gdalinfo /vsicurl/https://www.opengeodata.nrw.de/produkte/geobasis/lusat/dop/dop_jp2_f10/dop10rgbi_32_531_5744_1_nw_2022.jp2 > test.txt
+
 # overall: 35860 DOPs
 # Example: https://www.opengeodata.nrw.de/produkte/geobasis/lusat/dop/dop_jp2_f10/dop10rgbi_32_363_5619_1_nw.jp2
 # lynx -dump -nonumbers -listonly $URL | grep www.opengeodata.nrw.de/produkte/geobasis/lusat/dop/ | grep 'jp2$' | sed 's+^+/vsicurl/+g' > opengeodata_nrw_dop10_URLs.csv
