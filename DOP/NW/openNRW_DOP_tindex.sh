a24ef0de70b71b4523748a13e25088e2d7c19e38
anikaweinmann <aweinmann@mundialis.de>
2023-06-07 21:18:07 +0200

    try tindex creation 3

--- DOP/NW/openNRW_DOP_tindex.sh
+++ DOP/NW/openNRW_DOP_tindex.sh
@@ -50,8 +50,8 @@ echo "/vsicurl/https://www.opengeodata.nrw.de/produkte/geobasis/lusat/dop/dop_jp
 echo "/vsicurl/https://www.opengeodata.nrw.de/produkte/geobasis/lusat/dop/dop_jp2_f10/dop10rgbi_32_531_5745_1_nw_2022.jp2" >> opengeodata_nrw_dop10_URLs.csv
 
 # create tindex
-gdaltindex -f GPKG NW_DOP_tileindex.gpkg --optfile opengeodata_nrw_dop10_URLs.csv
-gzip NW_DOP_tileindex.gpkg
+gdaltindex -f GPKG NW_DOP_tileindex2.gpkg --optfile opengeodata_nrw_dop10_URLs.csv
+gzip NW_DOP_tileindex2.gpkg
 
 # cleanup
 rm opengeodata_nrw_dop10_URLs.csv
