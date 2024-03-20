79b35233440186ddacd7e0d6611403b99e3bb8a0
jhalbauer <j.halbauerjena@gmail.com>
2024-03-20 12:28:41 +0100

    updated NW DOP tileindex and its files

---
+++ DOP/NW/DOP_tileindex_NW.py
@@ -0,0 +1,91 @@
+############################################################################
+#
+# MODULE:      DOP_tileindex_NW
+# AUTHOR(S):   Johannes Halbauer
+#
+# PURPOSE:     Creats a DOP tile index for Nordrhein-Westfalen based on the file names of DOPs from https://www.opengeodata.nrw.de/produkte/geobasis/lusat/akt/dop/dop_jp2_f10/ stored in a .csv file.
+# COPYRIGHT:   (C) 2024 by mundialis GmbH & Co. KG
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#############################################################################
+
+
+# import required packages
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Polygon
+
+
+# define csv file with DOP URLs
+
+# All DOP download URLs stored in a .csv file are required. Each URL is
+# stored in a new line without any seperator between the lines.
+# Depending on the URLs length the indexing in line 33 and 34 should be adjusted.
+csv_file = "/path/to/csv/file.csv"
+
+# define path for tileindex GeoPackage
+geopackage = "/path/to/output/tileindex/geopackage.gpkg"
+
+# import csv of DOP URLs as list
+URLs = open(csv_file).readlines()
+
+# create lists for GeoDataframe
+attr_url = []
+polygons = []
+
+print("Extracting coordinates of DOP tiles...")
+for record in URLs:
+    # extract coordinates of lower left corner (sw), write them to list
+    east_sw = int(record[98:101]) * 1000
+    north_sw = int(record[102:106]) * 1000
+    corner_sw = (east_sw, north_sw)
+
+    # calculate coordinates for remaining corners (DOP = 1000 x 1000 m)
+    # lower right corner (se)
+    east_se = east_sw + 1000
+    north_se = north_sw
+    corner_se = (east_se, north_se)
+
+    # upper left corner (nw)
+    east_nw = east_sw
+    north_nw = north_se + 1000
+    corner_nw = (east_nw, north_nw)
+
+    # upper right corner (ne)
+    east_ne = east_sw + 1000
+    north_ne = north_sw + 1000
+    corner_ne = (east_ne, north_ne)
+
+    # create shapely polygon object
+    polygon = Polygon([corner_sw, corner_se, corner_ne, corner_nw])
+
+    # write polygon object to list
+    polygons.append(polygon)
+
+    # write download URL to list for becoming an attribute of the tileindex
+    attr_url.append(record)
+
+
+print("Creating dataframe and write it to GeoPackage...")
+
+# create dataframe including tile URL and corner coordinates
+columns = ["coordinates", "location"]
+df = pd.DataFrame(list(zip(polygons, attr_url)), columns=columns, index=None)
+
+
+# create GeoDataFrame out of DataFrame
+gdf = gpd.GeoDataFrame(df, geometry="coordinates", crs="EPSG:25832")
+
+# write GeoDataFrame to GeoPackage
+gdf.to_file(geopackage, layer="openNRW_DOP10_tileindex", driver="GPKG")
+
+print("Done!")
--- DOP/NW/README.md
+++ DOP/NW/README.md
@@ -1,5 +1,20 @@
 # Nordrhein-Westfalen (NRW/NW) Tile index of Digitales Orthophoto from openNRW data
 
 * openNRW data: Digitales Orthophoto, R-G-B-NIR, 10cm resolution, JP2000 format
-* [Scripts](https://github.com/mundialis/openNRW/tree/master/dop) to create the tile index
-* Download link: https://github.com/mundialis/tile-indices/raw/main/DOP/NW/openNRW_DOP10_tileindex.gpkg.gz
+
+Tile index:
+- `DOP20_tileindex_BE_BB.gpkg.gz`
+- contains one poylgon for every DOP
+- download URL for each poylgon is stored as attribute ("location")
+
+Create tileindex script:
+- `DOP_tileindex_NW.py`
+- extracts UTM coordinates out of DOP names
+- based on the southwest corner it calculates the DOP extents
+- requires a .csv file containing each download URL for all DOPs (no seperator, one URL per line) including the prefix "/vsicurl/"
+- requires an output file path (.gpkg)
+- depending on the URLs length the indexing in line 33 & 34 should be adjusted
+
+Links
+* Download link: https://github.com/mundialis/tile-indices/raw/main/DOP/NW/DOP10_tileindex_NW.gpkg.gz
+* [metadata, license, ...](https://www.geoportal.nrw/?activetab=map#/datasets/iso/56fb584b-10cf-4009-a405-0bef06bb3e00)
