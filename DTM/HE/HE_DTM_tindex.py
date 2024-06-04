38031036fb60819beccd871bf2b9ad8389e7efd5
anikaweinmann <aweinmann@mundialis.de>
2024-06-04 13:12:51 +0200

    LK review

--- DTM/HE/HE_DTM_tindex.py
+++ DTM/HE/HE_DTM_tindex.py
@@ -27,10 +27,11 @@
 ############################################################################
 
 # Usage inside grass location:
-#   grass -c epsg:25832 /grassdb/HE_ALKIS --exec python3 DTM/HE/HE_DTM_tindex.py
+#   grass -c epsg:25832 /grassdb/HE_DTM --exec python3 DTM/HE/HE_DTM_tindex.py
 # Output:
 #   DTM/HE/HE_DTM_tileindex.gpkg.gz
 
+import atexit
 import json
 import os
 import shutil
@@ -63,11 +64,19 @@ URL_GERMANY_DATA = (
     "https://daten.gdz.bkg.bund.de/produkte/vg/vg5000_0101/aktuell/"
     "vg5000_01-01.utm32s.shape.ebenen.zip"
 )
+# path where to the folder where the tindex should be created
+DTM_HE_PATH="/src/tile-indices/DTM/HE/"
+DTM_HE_PATH="DTM/HE/"  # locally
+
+# file names
+GEOJSON_NAME = "tindex.geojson"
+GPKG_NAME = "HE_DTM_tindex.gpkg"
 
 """FUNCTIONS"""
 
 
 def download_and_unzip(url, extract_to='.'):
+    """Download and unzip data"""
     ssl._create_default_https_context = ssl._create_unverified_context
     try:
         http_response = urlopen(url)
@@ -77,7 +86,7 @@ def download_and_unzip(url, extract_to='.'):
     zipfile.extractall(path=extract_to)
 
 
-def check_url(url):
+def check_url(url, date):
     """Function to check if url is reachable"""
     url_today = url.replace("DATE", date)
     test_url = 400
@@ -110,7 +119,6 @@ def check_url(url):
     if test_url == 200:
         return True
     else:
-        # grass.message(_(f"{url_today} is not reachable."))
         return False
 
 
@@ -164,173 +172,177 @@ def generate_gem_var(gem):
     return gem_vars
 
 
+def cleanup():
+    """Cleanup function"""
+    # cleanup
+    if os.path.isdir("tmp"):
+        shutil.rmtree("tmp")
+    if os.path.isfile(GEOJSON_NAME):
+        os.remove(GEOJSON_NAME)
+    if os.path.isfile(GPKG_NAME):
+        os.remove(GPKG_NAME)
+
 """MAIN PART"""
 
-os.chdir("/src/tile-indices/DTM/HE/")
-# os.chdir("DTM/HE/")
 
-if not os.path.isdir("tmp"):
-    os.makedirs("tmp")
+def main():
+    """Main function of tindex creation"""
+    os.chdir(DTM_HE_PATH)
 
-"""GET PARTS OF HESSEN (KREISE + GEMEINDEN)"""
+    if not os.path.isdir("tmp"):
+        os.makedirs("tmp")
 
-# downloading germany boundaries
-download_and_unzip(URL_GERMANY_DATA, "tmp")
+    """GET PARTS OF HESSEN (KREISE + GEMEINDEN)"""
 
-# importing Hessen
-file_path = os.path.join(
-    "tmp",
-    "vg5000_01-01.utm32s.shape.ebenen",
-    "vg5000_ebenen_0101",
-)
-grass.run_command(
-    "v.in.ogr",
-    input=os.path.join(file_path, "VG5000_LAN.shp"),
-    output="lan",
-    where=f"GEN='{FS}'",
-    quiet=True,
-    overwrite=True,
-)
-grass.run_command("g.region", vector="lan", flags="p")
+    # downloading germany boundaries
+    download_and_unzip(URL_GERMANY_DATA, "tmp")
 
-# importing Gemeinden and Kreise of Hessen
-grass.run_command(
-    "v.in.ogr",
-    input=os.path.join(file_path, "VG5000_KRS.shp"),
-    output="krs",
-    flags="r",
-    quiet=True,
-    overwrite=True,
-)
-grass.run_command(
-    "v.in.ogr",
-    input=os.path.join(file_path, "VG5000_GEM.shp"),
-    output="gem",
-    flags="r",
-    quiet=True,
-    overwrite=True,
-)
+    # importing Hessen
+    file_path = os.path.join(
+        "tmp",
+        "vg5000_01-01.utm32s.shape.ebenen",
+        "vg5000_ebenen_0101",
+    )
+    grass.run_command(
+        "v.in.ogr",
+        input=os.path.join(file_path, "VG5000_LAN.shp"),
+        output="lan",
+        where=f"GEN='{FS}'",
+        quiet=True,
+        overwrite=True,
+    )
+    grass.run_command("g.region", vector="lan", flags="p")
 
-# overlay of GEM and KRS
-overlay_tmp = f"{FS}_DTM_tmp"
-overlay = f"{FS}_DTM"
-grass.run_command(
-    "v.overlay",
-    ainput="krs",
-    binput="gem",
-    output=overlay_tmp,
-    operator="and",
-    quiet=True,
-    overwrite=True,
-)
-grass.run_command(
-    "v.clip",
-    input=overlay_tmp,
-    output=overlay,
-    clip="lan",
-    quiet=True,
-    overwrite=True,
-)
-vals = grass.vector_db_select(overlay, columns="a_BEZ,a_GEN,b_GEN")["values"]
-# cleanup columns of overlay
-columns = grass.vector_db_select(overlay)["columns"]
-columns.remove("cat")
-grass.run_command("v.db.dropcolumn", map=overlay, columns=columns)
-grass.run_command("v.db.addcolumn", map=overlay, columns="location TEXT")
+    # importing Gemeinden and Kreise of Hessen
+    grass.run_command(
+        "v.in.ogr",
+        input=os.path.join(file_path, "VG5000_KRS.shp"),
+        output="krs",
+        flags="r",
+        quiet=True,
+        overwrite=True,
+    )
+    grass.run_command(
+        "v.in.ogr",
+        input=os.path.join(file_path, "VG5000_GEM.shp"),
+        output="gem",
+        flags="r",
+        quiet=True,
+        overwrite=True,
+    )
 
-"""CREATE DICT WITH TILES FOR"""
+    # overlay of GEM and KRS
+    overlay_tmp = f"{FS}_DTM_tmp"
+    overlay = f"{FS}_DTM"
+    grass.run_command(
+        "v.overlay",
+        ainput="krs",
+        binput="gem",
+        output=overlay_tmp,
+        operator="and",
+        quiet=True,
+        overwrite=True,
+    )
+    grass.run_command(
+        "v.clip",
+        input=overlay_tmp,
+        output=overlay,
+        clip="lan",
+        quiet=True,
+        overwrite=True,
+    )
+    vals = grass.vector_db_select(overlay, columns="a_BEZ,a_GEN,b_GEN")["values"]
+    # cleanup columns of overlay
+    columns = grass.vector_db_select(overlay)["columns"]
+    columns.remove("cat")
+    grass.run_command("v.db.dropcolumn", map=overlay, columns=columns)
+    grass.run_command("v.db.addcolumn", map=overlay, columns="location TEXT")
 
-# create GeoJson
-geojson_dict = {
-  "type": "FeatureCollection",
-  "name": "tindex",
-  "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:EPSG::25832"}},
-  "features": []
-}
+    """CREATE DICT WITH TILES FOR"""
 
-# find urls for each gem
-date = datetime.now().strftime("%Y%m%d")
-ALKIS_urls = {}
-krs_gem = {}
-krs_base_urls = {}
-num_HE_gem = len(vals)
-i = 0
-count = 0
-for cat, val in vals.items():
-    i += 1
-    if i % 10 == 0:
-        grass.message(_(f"{i}/{num_HE_gem} ..."))
-    krs_type = val[0]
-    krs = val[1]
-    gem = val[2]
+    # create GeoJson
+    geojson_dict = {
+    "type": "FeatureCollection",
+    "name": "tindex",
+    "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:EPSG::25832"}},
+    "features": []
+    }
 
-    # create url for krs and gem
-    print(type, krs, gem)
-    poss_krs_urls = create_urls(krs_type, krs, gem)
+    # find urls for each gem
+    date = datetime.now().strftime("%Y%m%d")
+    num_HE_gem = len(vals)
+    i = 0
+    count = 0
+    for cat, val in vals.items():
+        i += 1
+        if i % 10 == 0:
+            grass.message(_(f"{i}/{num_HE_gem} ..."))
+        krs_type = val[0]
+        krs = val[1]
+        gem = val[2]
 
-    url = None
-    for poss_krs_url in set(poss_krs_urls):
-        if check_url(poss_krs_url):
-            url_today = poss_krs_url.replace("DATE", date)
-            tries = 0
-            zip = None
-            while tries <= MAX_RETRIES and zip is None:
-                try:
-                    zip = RemoteZip(url_today, timeout=1200)
-                except:
-                    sleep(15)
-                # grass.fatal(f"Error with RemoteZip for url <{url_today}> !")
-            zip_files = [zip_info.filename for zip_info in zip.infolist() if zip_info.filename.endswith(".tif")]
-            for zip_file in zip_files:
-                url = f"/vsizip/vsicurl/{poss_krs_url}/{zip_file}"
-                splitted_file_name = zip_file.split("_")
-                x1 = int(splitted_file_name[2]) * 1000
-                y1 = int(splitted_file_name[3]) * 1000
-                x2 = x1 + 1000
-                y2 = y1 + 1000
-                feat = {
-                    "type": "Feature", "properties": {
-                        "fid": count + 1,
-                        "location": url
-                    },
-                    "geometry": {
-                        "type": "Polygon", "coordinates": [[[x1, y1], [x2, y1], [x2, y2], [x1, y2], [x1, y1]]]
+        # create url for krs and gem
+        print(type, krs, gem)
+        poss_krs_urls = create_urls(krs_type, krs, gem)
+
+        url = None
+        for poss_krs_url in set(poss_krs_urls):
+            if check_url(poss_krs_url, date):
+                url_today = poss_krs_url.replace("DATE", date)
+                tries = 0
+                zip = None
+                while tries <= MAX_RETRIES and zip is None:
+                    try:
+                        zip = RemoteZip(url_today, timeout=1200)
+                    except:
+                        sleep(15)
+                    # grass.fatal(f"Error with RemoteZip for url <{url_today}> !")
+                zip_files = [zip_info.filename for zip_info in zip.infolist() if zip_info.filename.endswith(".tif")]
+                for zip_file in zip_files:
+                    url = f"/vsizip/vsicurl/{poss_krs_url}/{zip_file}"
+                    splitted_file_name = zip_file.split("_")
+                    x1 = int(splitted_file_name[2]) * 1000
+                    y1 = int(splitted_file_name[3]) * 1000
+                    x2 = x1 + 1000
+                    y2 = y1 + 1000
+                    feat = {
+                        "type": "Feature", "properties": {
+                            "fid": count + 1,
+                            "location": url
+                        },
+                        "geometry": {
+                            "type": "Polygon", "coordinates": [[[x1, y1], [x2, y1], [x2, y2], [x1, y2], [x1, y1]]]
+                        }
                     }
-                }
-                geojson_dict["features"].append(feat)
-                count += 1
-    if url is None:
-        grass.fatal(f"No url found for {krs_type, krs, gem}!")
+                    geojson_dict["features"].append(feat)
+                    count += 1
+        if url is None:
+            grass.fatal(f"No url found for {krs_type, krs, gem}!")
 
-"""CREATE TILE INDEX AS .gpkg.gz FILE"""
+    """CREATE TILE INDEX AS .gpkg.gz FILE"""
 
-# create GeoJson tile index
-geojson_name = "tindex.geojson"
-with open(geojson_name, "w") as f:
-    json.dump(geojson_dict, f, indent=4)
+    # create GeoJson tile index
+    with open(GEOJSON_NAME, "w") as f:
+        json.dump(geojson_dict, f, indent=4)
 
-# create GPKG from GeoJson
-gpkg_name = "HE_DTM_tindex.gpkg"
-stream = os.popen(f"ogr2ogr {gpkg_name} {geojson_name}")
-ogr2ogr_out = stream.read()
+    # create GPKG from GeoJson
+    stream = os.popen(f"ogr2ogr {GPKG_NAME} {GEOJSON_NAME}")
+    stream.read()
 
-# verify
-print("Verifying vector tile index:")
-stream = os.popen(f"ogrinfo -so -al {gpkg_name}")
-tindex_verification = stream.read()
-print(tindex_verification)
+    # verify
+    print("Verifying vector tile index:")
+    stream = os.popen(f"ogrinfo -so -al {GPKG_NAME}")
+    tindex_verification = stream.read()
+    print(tindex_verification)
 
-# package
-if os.path.isfile(f"{gpkg_name}.gz"):
-    os.remove(f"{gpkg_name}.gz")
-stream = os.popen(f"gzip {gpkg_name}")
-create_gz = stream.read()
-print(f"<{gpkg_name}.gz> created")
+    # package
+    if os.path.isfile(f"{GPKG_NAME}.gz"):
+        os.remove(f"{GPKG_NAME}.gz")
+    stream = os.popen(f"gzip {GPKG_NAME}")
+    stream.read()
+    print(f"<{GPKG_NAME}.gz> created")
 
-# cleanup
-if os.path.isdir("tmp"):
-    shutil.rmtree("tmp")
-if os.path.isfile(geojson_name):
-    os.remove(geojson_name)
-if os.path.isfile(gpkg_name):
-    os.remove(gpkg_name)
+
+if __name__ == "__main__":
+    atexit.register(cleanup)
+    main()
