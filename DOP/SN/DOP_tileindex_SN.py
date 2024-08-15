2a901c0c7b6bb52acc7abbb79a24fe1232adc1dc
jhalbauer <j.halbauerjena@gmail.com>
2024-08-15 14:41:08 +0200

    added DOP SN workflow for automated tindex creation

--- DOP/SN/DOP_tileindex_SN.py
+++ DOP/SN/DOP_tileindex_SN.py
@@ -1,10 +1,16 @@
 ############################################################################
 #
-# MODULE:      DOP_tileindex_SN
-# AUTHOR(S):   Johannes Halbauer
+# MODULE:       DOP_tileindex_SN
 #
-# PURPOSE:     Creates a DOP tile index for Sachsen based on the file names of DOPs from https://www.geodaten.sachsen.de/index.html stored in a .csv file.
-# COPYRIGHT:   (C) 2023 by mundialis GmbH & Co. KG
+# AUTHOR(S):    Johannes Halbauer
+#
+# PURPOSE:      Creates a DOP tile index for Sachsen based on the file names of DOPs from https://www.geodaten.sachsen.de/index.html stored in a .csv file.
+#               
+# Data source:  https://www.geodaten.sachsen.de/batch-download-4719.html
+#
+# COPYRIGHT:    (C) 2023 by mundialis GmbH & Co. KG
+#
+# REQUIREMENTS: pandas, geopandas, shapely
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,22 +25,21 @@
 #############################################################################
 
 
-# import required packages
+import gzip
+import os
+import shutil
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Polygon
-import csv
 
+os.chdir("DOP/SN/")
 
 # define csv file with DOP URLs
-
-# All DOP download URLs stored in a .csv file are required. Each URL is
-# stored in a new line without any seperator between the lines.
-# Depending on the URLs length the indexing in line 33 and 34 should be adjusted.
-csv_file = "/path/to/csv/file"
+csv_file = "SN_DOP20_URLs.csv"
 
 # define path for tileindex GeoPackage
-geopackage = "path/to/output/tileindex/geopackage"
+geopackage = "DOP20_tileindex_SN.gpkg"
+geopackage_compr = f"{geopackage}.gz"
 
 # import csv of DOP URLs as list
 URLs = open(csv_file).readlines()
@@ -44,6 +49,7 @@ attr_url = []
 polygons = []
 
 print("Extracting coordinates of DOP tiles...")
+
 for record in URLs:
     #extract coordinates of lower left corner (sw), write them to list
     east_sw = (int(record[125:128]) * 1000)
@@ -75,13 +81,11 @@ for record in URLs:
     # write download URL to list for becoming an attribute of the tileindex
     attr_url.append(record)
 
-
 print("Creating dataframe and write it to GeoPackage...")
 
 # create dataframe including tile URL and corner coordinates
 columns = ["coordinates", "location"]
 df = pd.DataFrame(list(zip(polygons, attr_url)), columns=columns, index=None)
-
 
 # create GeoDataFrame out of DataFrame
 gdf = gpd.GeoDataFrame(df, geometry="coordinates", crs="EPSG:25833")
@@ -89,4 +93,13 @@ gdf = gpd.GeoDataFrame(df, geometry="coordinates", crs="EPSG:25833")
 # write GeoDataFrame to GeoPackage
 gdf.to_file(geopackage, layer="geoportalSN_DOP20_tileindex", driver="GPKG")
 
-print("Done!")
+# compress GeoPackage
+if os.path.isfile(geopackage_compr):
+    os.remove(geopackage_compr)
+with open(geopackage, "rb") as f_in:
+    with gzip.open(geopackage_compr, "wb") as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+# cleanup
+if os.path.isfile(geopackage):
+    os.remove(geopackage)
