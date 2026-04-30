85cb0c0d941cd6cddefdbf7c97004ea9ad52349c
Kim Kaiser <kim.m.u.kaiser@gmail.com>
2026-04-30 15:12:42 +0200

    Linting and added README

--- DTM/HB/HB_DTM_tindex.py
+++ DTM/HB/HB_DTM_tindex.py
@@ -1,10 +1,8 @@
 ############################################################################
 #
 # MODULE:      HB_DTM_tindex
-# AUTHOR(S):   Kim Kaiser, Anika Weinmann
-# PURPOSE:     Creates a DTM tile index for Bremen and Bremerhaven based on the file names
-#              of DTMs from https://www.metaver.de/trefferanzeige?docuuid=2351ABA6-019D-4155-853F-76EEFC26CA52
-#              
+# AUTHOR(S):   Anika Weinmann, Kim Kaiser
+# PURPOSE:     Creates a tile index of Bremen and Bremerhaven DTM files
 # SPDX-FileCopyrightText: (c) 2026 by mundialis GmbH & Co. KG and the
 #                             GRASS Development Team
 # SPDX-License-Identifier: GPL-3.0-or-later.
@@ -14,12 +12,16 @@
 import os
 import json
 
-from osgeo import gdal
 from remotezip import RemoteZip
 
 # Set Parameter
-# Bremen und Bremerhaven
-URLS = ["https://gdi2.geo.bremen.de/inspire/download/DGM/data/Gitternetz_DGM1_2017_HB_ASCII_XYZ.zip", "https://gdi2.geo.bremen.de/inspire/download/DGM/data/Gitternetz_DGM1_2015_BHV_ASCII_XYZ.zip"]
+# Separate URLs for Bremen und Bremerhaven
+URLS = [
+    "https://gdi2.geo.bremen.de/inspire/download/DGM/data/"
+    "Gitternetz_DGM1_2017_HB_ASCII_XYZ.zip",
+    "https://gdi2.geo.bremen.de/inspire/download/DGM/data/"
+    "Gitternetz_DGM1_2015_BHV_ASCII_XYZ.zip",
+]
 EPSG_CODE = 25832
 FILE_EXTENSION = ".xyz"
 TILE_SIZE = 1000
@@ -36,13 +38,13 @@ def create_tindex_by_filename(data_list):
             "type": "name",
             "properties": {"name": f"urn:ogc:def:crs:EPSG::{EPSG_CODE}"},
         },
-        "features": []
+        "features": [],
     }
 
     for num, data in enumerate(data_list):
-        splitted_data_name = os.path.basename(
-            data
-        ).replace(FILE_EXTENSION, "").split("_")
+        splitted_data_name = (
+            os.path.basename(data).replace(FILE_EXTENSION, "").split("_")
+        )
         if "" in splitted_data_name:
             splitted_data_name.remove("")
         y1 = int(splitted_data_name[2]) * 1000
@@ -50,12 +52,14 @@ def create_tindex_by_filename(data_list):
         y2 = y1 + TILE_SIZE
         x2 = x1 + TILE_SIZE
         feat = {
-            "type": "Feature", "properties": {
+            "type": "Feature",
+            "properties": {
                 "fid": num + 1,
                 "location": data,
             },
             "geometry": {
-                "type": "Polygon", "coordinates": [[
+                "type": "Polygon",
+                "coordinates": [[
                     [x1, y1],
                     [x2, y1],
                     [x2, y2],
@@ -69,21 +73,23 @@ def create_tindex_by_filename(data_list):
     with open("tindex.geojson", "w") as f:
         json.dump(geojson_dict, f, indent=4)
 
-     # create GPKG from GeoJson
+    # create GPKG from GeoJson
     tindex_gpkg = OUTPUT_FILE.rsplit(".", 1)[0]
     stream = os.popen(f"ogr2ogr {tindex_gpkg} tindex.geojson")
     stream.read()
     return tindex_gpkg
+
 
 # get XYZ data list
 data_list = []
 for URL in URLS:
     with RemoteZip(URL) as zip:
         for zip_info in zip.infolist():
-            file_name = zip_info.filename
+            # Skips subfolders
+            split_file_name = zip_info.filename.split("/")
+            file_name = split_file_name[-1]
             if file_name.endswith(FILE_EXTENSION):
                 data_list.append(file_name)
-import pdb; pdb.set_trace()
 # create tindex
 tindex_gpkg = create_tindex_by_filename(data_list)
 
@@ -105,6 +111,3 @@ if os.path.isfile("tindex.geojson"):
     os.remove("tindex.geojson")
 if os.path.isfile(tindex_gpkg):
     os.remove(tindex_gpkg)
-
-
-
